7a8c30002a2483ade3708b263327961cc9dd0bf1
Varad4564 <100591815+Varad4564@users.noreply.github.com>
2022-04-08 21:44:45 +0530

    Add files via upload

---
+++ Fibonacci.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+public class Fibonacci {
+    public static void main(String args[]){
+        int firstTerm=0,secondTerm=1;
+        Scanner sc=new Scanner (System.in);
+        int n=sc.nextInt();
+        System.out.println("Fibonacci series upto"+n+":");
+        while(firstTerm<=n){
+            System.out.print(firstTerm+",");
+            int nextTerm=firstTerm+secondTerm;
+            firstTerm=secondTerm;
+            secondTerm=nextTerm;
+        }
+        sc.close();
+    }
+}
